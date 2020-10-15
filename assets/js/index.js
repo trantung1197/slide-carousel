63aa4ab0d32627b58a6b0303f23b6cb6f602b187
Tran Tung <trantung1197@gmail.com>
2020-10-15 20:19:57 +0700

    create git

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImg = document.querySelectorAll('.slide-item img');
+const btnPrev = document.getElementById('btn-prev');
+const btnNext = document.getElementById('btn-next');
+
+
