2dcbe29b4ea86eaf5e6048f2ebde2aaaec8da943
Tran Tung <trantung1197@gmail.com>
2020-10-16 13:26:33 +0700

    file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,25 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImg = document.querySelectorAll('.slide-item img');
-const btnPrev = document.getElementById('btn-prev');
-const btnNext = document.getElementById('btn-next');
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselItem = document.querySelectorAll(".slide-item");
+const btnPrev = document.getElementById("btn-prev");
+const btnNext = document.getElementById("btn-next");
+
+let indexActive = 1;
+const size = carouselItem[0].clientWidth;
+let timeTrans = 0.4;
+
+
+btnNext.addEventListener("click", () => {
+    console.log(carouselSlide);
+    carouselSlide.style.transition = `transform ${timeTrans}s ease-in-out`;
+    indexActive++;
+    carouselSlide.style.transform = `translateX(${-size * indexActive}px)`;
+});
+
+btnPrev.addEventListener("click", () => {
+    console.log(carouselSlide);
+    carouselSlide.style.transition = `all ${timeTrans}s linear`;
+    indexActive--;
+    carouselSlide.style.transform = `translateX(${-size * indexActive}px)`;
+});
 
 
